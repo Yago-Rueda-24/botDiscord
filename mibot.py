6840aa75f99239ee45f5abcac2e99e897d0a3899
yago-vasco <yago.rc.04@gmail.com>
2025-04-27 22:13:13 +0200

    Se han modificado los comandos para implemetar comandos slash

--- mibot.py
+++ mibot.py
@@ -7,35 +7,40 @@ from discord.ext import commands
 from discord import app_commands
 
 
+
 import discord
 
 
 intents = discord.Intents.default()
 intents.message_content = True
-
+GUILD_ID = discord.Object(id = 1365653490055254038)
 client = commands.Bot(command_prefix='$', intents=intents)
 
 
 @client.event
 async def on_ready():
     print(f'We have logged in as {client.user}')
+    try:
+        synced = await client.tree.sync(guild=GUILD_ID)  
+        print(f'Slash Commands sincronizados: {len(synced)}')
+    except Exception as e:
+        print(f'Error al sincronizar Slash Commands: {e}')
 
-@client.event
-async def on_message(message):
-    if message.author == client.user:
-        return
-
-    if message.content.startswith('$hola'):
-        await message.channel.send('Hola, estoy conectado y listo para el servicio')
-    if message.content.startswith('$help'):
-        embed = discord.Embed(
+@client.tree.command(name="hola", description="Saluda al bot", guild=GUILD_ID)
+async def hola(interaction: discord.Interaction):
+    await interaction.response.send_message(f'¡Hola, {interaction.user.name}, estoy conectado y listo para el servicio')
+    
+@client.tree.command(name="help", description="Muestra la ayuda del bot", guild=GUILD_ID)
+async def help(interaction: discord.Interaction):
+    embed = discord.Embed(
             title="Lista de Comandos",
             description="Aquí tienes los comandos que puedes usar:",
             color=discord.Color.green()
-        )
-        embed.add_field(name="$hola", value="Saludar al bot.", inline=False)
-        embed.add_field(name="$help", value="Mostrar este mensaje de ayuda.", inline=False)
-        embed.set_footer(text="Si necesitas ayuda con algun comando, escribelo sin argumentos para que este te de la ayuda")
-        await message.channel.send(embed=embed)
+    )
+    embed.add_field(name="/hola", value="Saludar al bot.", inline=False)
+    embed.add_field(name="/help", value="Mostrar este mensaje de ayuda.", inline=False)
+    embed.set_footer(text="Si necesitas ayuda con algun comando, escribelo sin argumentos para que este te de la ayuda")
         
+    await interaction.response.send_message(embed=embed)
+                
 client.run(entorno.TOKEN)
