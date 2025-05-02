80cc026a67e0fef1bed20b787b9a132b422b7a7e
yago-vasco <yago.rc.04@gmail.com>
2025-05-02 14:36:12 +0200

    Se ha implemenado el comando de lanzar una moneda

---
+++ cogs/Aleatorios.py
@@ -0,0 +1,32 @@
+import discord
+from time import sleep
+from random import randint
+from discord.ext import commands
+from discord.ext import commands
+from discord import app_commands
+import entorno
+
+GUILD_ID = discord.Object(id = entorno.GUILD_ID)
+class prueba(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    @app_commands.command(name="moneda",description="Lanza una moneda y anuncia el resultado")
+    async def moneda(self,interaction: discord.Interaction):
+        if(randint(0,1)==0):
+            await interaction.response.send_message(f'Ha salido: CRUZ')
+        else:
+            await interaction.response.send_message(f'Ha salido: CARA')
+            
+    async def dado(self,interaction: discord.Interaction,message:str):
+        caras = int(message)
+        
+        await interaction.response.send_message(f'¡Hola, {interaction.user.name}, estoy conectado y listo para el servicio')
+        
+    async def cog_load(self):
+        #Registro de comnados en el arbol del cog actual
+        self.bot.tree.add_command(self.moneda, guild=GUILD_ID)
+
+# Añadir el cog al bot
+async def setup(bot):
+    await bot.add_cog(prueba(bot))
