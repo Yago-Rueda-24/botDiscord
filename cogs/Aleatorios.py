e0fd430856c9c18ad29112eb05ef6ce0599ac116
Yago <yago.rc.04@gmail.com>
2025-05-05 12:20:58 +0200

    Se ha implementado el comando de ruleta

--- cogs/Aleatorios.py
+++ cogs/Aleatorios.py
@@ -28,10 +28,47 @@ class prueba(commands.Cog):
             else:
                 resultado = randint(1,caras)
                 await interaction.response.send_message(f'El resultado del dado de un d{caras} es: {resultado}')
+    async def ruleta(self,interaction: discord.Interaction):
+        # Números rojos en la ruleta europea
+        rojos = [1, 3, 5, 7, 9, 12, 14, 16, 18,
+         19, 21, 23, 25, 27, 30, 32, 34, 36]
         
+        resultado = randint(0,36)
+        if resultado == 0:
+            embed = discord.Embed(
+            title="Resultado de la ruleta",
+            description="El resultado de la ruleta es: 0",
+            color=discord.Color.green()
+            )
+            await interaction.response.send_message(embed=embed)
+        else:
+            # Determinar la paridad
+            if resultado % 2 == 0:
+                paridad = "par"
+            else:
+             paridad = "impar"
+            # Determinar el color
+            if resultado in rojos:
+                color = "rojo"
+            else:
+                color = "negro"
+
+            embed = discord.Embed(
+            title="Resultado de la ruleta",
+            description=f"El resultado de la ruleta es: {resultado}", 
+            color=discord.Color.red() if color == "rojo" else discord.Color.dark_grey()
+
+            )
+            embed.add_field(name="El número es {paridad}",inline=False)
+            embed.add_field(name="El color es {color}",inline=False)
+            await interaction.response.send_message(embed=embed)
+
+
     async def cog_load(self):
         #Registro de comnados en el arbol del cog actual
         self.bot.tree.add_command(self.moneda, guild=GUILD_ID)
+        self.bot.tree.add_command(self.dado, guild=GUILD_ID)    
+        self.bot.tree.add_command(self.ruleta, guild=GUILD_ID)
 
 # Añadir el cog al bot
 async def setup(bot):
