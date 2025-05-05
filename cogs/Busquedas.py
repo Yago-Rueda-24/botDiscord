ee52c09d4958b36d46460940ee70532197037a3f
Yago <yago.rc.04@gmail.com>
2025-05-05 12:33:47 +0200

    Se ha creado el esqueleto de la función de busquedas en google

--- cogs/Busquedas.py
+++ cogs/Busquedas.py
@@ -4,14 +4,19 @@ from random import randint
 from discord.ext import commands
 from discord.ext import commands
 from discord import app_commands
+from google import genai
 import entorno
 
 GUILD_ID = discord.Object(id = entorno.GUILD_ID)
 class busquedas(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
+    @app_commands.command(name="busqueda",description="Realiza una busqueda en google")
+    async def google_search(self,interaction: discord.Interaction, message: str):
+        
+        await interaction.response.send_message(f'Buscando en Google: {message}')
 
-async def cog_load(self):
+    async def cog_load(self):
         #Registro de comnados en el arbol del cog actual
         return
 
