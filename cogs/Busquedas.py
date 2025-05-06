83964b5bfb8994a9070593f7cfdd9b3c54e1d375
yago-vasco <yago.rc.04@gmail.com>
2025-05-07 00:24:04 +0200

    Se ha implementado un comando que permite buscar cartas de magic the gathering

--- cogs/Busquedas.py
+++ cogs/Busquedas.py
@@ -1,6 +1,8 @@
 import discord
 from time import sleep
 from random import randint
+import requests
+import json
 from discord.ext import commands
 from discord.ext import commands
 from discord import app_commands
@@ -10,6 +12,7 @@ import entorno
 GEMINI_KEY = entorno.GEMINI_KEY
 GUILD_ID = discord.Object(id = entorno.GUILD_ID)
 client = genai.Client(api_key=GEMINI_KEY)
+
 class busquedas(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
@@ -18,9 +21,27 @@ class busquedas(commands.Cog):
     async def google_search(self,interaction: discord.Interaction, message: str):
         response =client.models.generate_content(model="gemini-2.0-flash", contents=message)
         await interaction.response.send_message(f'Buscando en Google: {response.text}')
+    
+    @app_commands.command(name="magic",description="Busca una carta de magic the gathering por su nombre")
+    async def magic_search(self,interaction: discord.Interaction, message: str):
+        url = f"https://api.scryfall.com/cards/named?fuzzy={message}"
+        response = requests.get(url)
+        if response.status_code == 200:
+            data = response.json()
+            card_name = data['name']
+            card_image = data['image_uris']['normal']
+            card_set = data['set_name']
+            card_type = data['type_line']
+            card_text = data['oracle_text']
+            embed = discord.Embed(title=card_name, description=f"Set: {card_set}\nType: {card_type}\nText: {card_text}")
+            embed.set_image(url=card_image)
+            await interaction.response.send_message(embed=embed)
+        else:
+            await interaction.response.send_message(f'No se encontró la carta: {message}')
 
     async def cog_load(self):
        self.bot.tree.add_command(self.google_search, guild=GUILD_ID)
+       self.bot.tree.add_command(self.magic_search, guild=GUILD_ID)
 
     
 # Añadir el cog al bot
